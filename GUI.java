ba311ab4d8d965de90b04fdfb008c371455241c9
danpalmer1 <xdanielpalmerx@gmail.com>
2023-01-30 16:50:15 -0600

    Added GUI class

---
+++ GUI.java
@@ -0,0 +1,23 @@
+import java.awt.*;
+import java.awt.event.*;
+
+public class GUI extends Frame {
+  public GUI() {
+    Button btn = new Button("Button");
+    btn.setBounds(50, 50, 50, 50);
+    add(btn);
+    setSize(150, 150);
+    setTitle("4 Board Game");
+    setLayout(new FlowLayout());
+    setVisible(true);
+    addWindowListener(new WindowAdapter() {
+        public void windowClosing(WindowEvent we) {
+            dispose();
+        }
+    });
+  }
+
+  public static void main(String args[]){
+    new GUI();
+  }
+}
