343a17933ea78842713bc70e89fcb1b76b306009
danpalmer1 <xdanielpalmerx@gmail.com>
2023-02-01 21:16:20 -0600

    Assigned random numbers to each pnSet

--- Game.java
+++ Game.java
@@ -1,11 +1,27 @@
+import java.util.ArrayList;
+
 public class Game {
     
-    int[] p1Set, p2Set, p3Set, p4Set; //unique to each game
+    ArrayList<Integer> p1Set, p2Set, p3Set, p4Set; //unique to each game
     int gameInSet; 
 
     public Game(Player p1, Player p2, Player p3, Player p4, int gameInSet) {
         this.gameInSet = gameInSet;
+        //output for testing
         System.out.println("Game " + gameInSet + " out of 10 has begun.");
-        //1.3 randomly assign numbers 1-20 to each players # set
+        //1.3 randomly assign 3 numbers 1-20 to each players # set
+        //init 4 empty array lists
+        p1Set = new ArrayList<>(0); p2Set = new ArrayList<>(0); p3Set = new ArrayList<>(0); p4Set = new ArrayList<>(0);
+        ArrayList<ArrayList<Integer>> arr = new ArrayList<>(4); 
+        arr.add(p1Set); arr.add(p2Set); arr.add(p3Set); arr.add(p4Set);
+        int listC = 1; //tracks the count of lists
+        for(ArrayList<Integer> set : arr) {
+            for(int j = 0; j < 3; j++) {
+                set.add((int)(Math.random()*20 + 1));
+            }
+            //output for testing
+            System.out.println("List " + listC + " created with " + set.toString()); 
+            listC++;
+        }
     }
 }
