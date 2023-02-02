15e0301e07c845b3f6ec810c1dc88830f9e1d380
danpalmer1 <xdanielpalmerx@gmail.com>
2023-02-01 19:21:07 -0600

    Created Player class with score attribute

---
+++ Player.java
@@ -0,0 +1,9 @@
+public class Player {
+    
+    //score is unique to each player and needs to adjusted between games
+    int score;
+
+    public Player() {
+        this.score = 0; //initialize score as 0
+    }
+}
