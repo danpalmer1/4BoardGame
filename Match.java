0ec46fc85d978e24c5c8808c563664fb831ac471
danpalmer1 <xdanielpalmerx@gmail.com>
2023-02-12 15:24:13 -0600

    initiated player objects in the match

--- Match.java
+++ Match.java
@@ -3,6 +3,11 @@ public class Match {
     Player p1, p2, p3, p4;
 
     public Match() {
+        //initialize players
+        this.p1 = new Player();
+        this.p2 = new Player();
+        this.p3 = new Player();
+        this.p4 = new Player();
         //run 10 instances of the game object to reflect a match
         for(int i = 1; i <= 10; i++) {
             new Game(p1, p2, p3, p4, i);
