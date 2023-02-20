5a9f70982df576e61aa6ca25d9fc5128bf4491f4
danpalmer1 <xdanielpalmerx@gmail.com>
2023-02-19 21:11:40 -0600

    runs gui now

--- Main.java
+++ Main.java
@@ -11,7 +11,9 @@ public class Main {
                 Player p2 = new Player();
                 Player p3 = new Player();
                 Player p4 = new Player();
-                Game game = new Game (p1, p2, p3, p4, 1);  
-                game.playGame();
+                Game game = new Game (p1, p2, p3, p4, 1); 
+                Match match = new Match(); 
+                GUI gui = new GUI(match);
+               // game.playGame();
         }
 }
